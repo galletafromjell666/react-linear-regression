8f2d699103644249f6f8fd21266d3c03d0e76376
Giovanni Aguirre <61204361+galletafromjell666@users.noreply.github.com>
2023-03-26 21:24:08 -0600

    Optimize NewPoint Form

--- src/components/NewPoint.jsx
+++ src/components/NewPoint.jsx
@@ -1,42 +1,46 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 import { addPoint } from '../features/regression/regressionSlice';
+
 const NewPoint = () => {
-    const dispatch = useDispatch();
-    const [valX, setValX] = useState(0);
-    const [errorValX, setErrorValX] = useState(false);
-    const [valY, setValY] = useState(0);
-    const [errorValY, setErrorValY] = useState(false);
+  const dispatch = useDispatch();
+  const [values, setValues] = useState({ valX: 0, errorValX: false, valY: 0, errorValY: false });
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setErrorValX(false);
-        setErrorValY(false);
-        if (isNaN(valX) || isNaN(valY)) {
-            //console.log('oh no');
-            isNaN(valX) && setErrorValX(true);
-            isNaN(valY) && setErrorValY(true);
-            return;
-        }
-        //console.log(`valX${valX} valY ${valY}`);
-        dispatch(addPoint({ x: valX, y: valY }));
-    };
+  const handleSubmit = useCallback((e) => {
+    e.preventDefault();
+    setValues({ ...values, errorValX: false, errorValY: false });
+    if (!/^-?\d*\.?\d+$/.test(values.valX) || !/^-?\d*\.?\d+$/.test(values.valY)) {
+      setValues({ ...values, errorValX: isNaN(values.valX), errorValY: isNaN(values.valY) });
+      return;
+    }
+    const { valX, valY } = values;
+    dispatch(addPoint({ x: parseFloat(valX), y: parseFloat(valY) }));
+  }, [dispatch, values]);
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                {errorValX && <h1>ERROR at X = NaN</h1>}
-                X=
-                <input id="valueOfX" value={valX} onChange={(event) => setValX(event.target.value.replace(/[^0-9+\-.]/g, ''))} required />
-            </div>
-            <div>
-                {errorValY && <h1>ERROR at Y = NaN</h1>}
-                Y=
-                <input id="valueOfY" value={valY} onChange={(event) => setValY(event.target.value.replace(/[^0-9+\-.]/g, ''))} required />
-            </div>
-            <input value="add" type="submit" />
-        </form>
-    );
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        {values.errorValX || values.errorValY ? <h1>ERROR: Invalid input</h1> : null}
+        X=
+        <input
+          id="valueOfX"
+          value={values.valX}
+          onChange={(event) => setValues({ ...values, valX: event.target.value.replace(/[^0-9+\-.]/g, '') })}
+          required
+        />
+      </div>
+      <div>
+        Y=
+        <input
+          id="valueOfY"
+          value={values.valY}
+          onChange={(event) => setValues({ ...values, valY: event.target.value.replace(/[^0-9+\-.]/g, '') })}
+          required
+        />
+      </div>
+      <input type="submit" value="add" />
+    </form>
+  );
 };
 
 export default NewPoint;
